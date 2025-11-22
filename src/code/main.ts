aa5ea6d0b108f63d97e528ff57dd800547098209
Abhinav Adhikari <abhinavadhikari87@gmail.com>
2025-11-22 23:09:32 +0545

    Done styling
    I need to screenshot the telemedical app. But except for that this is
    good for now.

--- src/code/main.ts
+++ src/code/main.ts
@@ -1,16 +1,19 @@
-const menu_toggle = document.getElementById("menu-toggle")
-const menu = document.getElementById("menu");
+function menu_toogle_intialized() {
+    const menu_toggle = document.getElementById("menu-toggle")
+    const menu = document.getElementById("menu");
 
-if (!menu) {
-    console.error("Couldn't find menu");
-    throw Error()
+    if (!menu) {
+        console.error("Couldn't find menu");
+        throw Error()
+    }
+
+    if (!menu_toggle) {
+        console.error("Couldn't find menu-toggle");
+        throw Error()
+    }
+
+    menu_toggle.onclick = () => {
+        menu.classList.toggle('invisible');
+    }
 }
 
-if (!menu_toggle) {
-    console.error("Couldn't find menu-toggle");
-    throw Error()
-}
-
-menu_toggle.onclick = () => {
-    menu.classList.toggle('invisible');
-}
